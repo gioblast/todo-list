12f6e86bd2243dc28ac8d9d0a3384cfb2a001c30
GiovanniFtuesta <giovanituesta@gmail.com>
2024-03-07 10:05:01 -0500

    updates in Todo-list
    Se han agregado el botón de eliminar y editar las tareas

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -5,13 +5,19 @@ namespace App\Livewire;
 use App\Models\Todo;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
     #[Rule('required|min:3|max:50')]
     public $name;
 
     public $search;
+
+    public $editingtodoID;
+    public $editingTodoName;
 
     public function create(){
         //validate
@@ -26,8 +32,25 @@ class TodoList extends Component
         session()->flash('success','creado.');
     }
 
+    public  function delete($todoID){
+        Todo::find($todoID)->delete();
+    }
+
+    public function toogle($todoID){
+        $todo = Todo::find($todoID);
+        $todo->completed = !$todo->completed;
+        $todo->save();
+    }
+
+    public function edit($todoID){
+        $this->editingtodoID = $todoID;
+        $this->editingTodoName = Todo::find($todoID)->name;
+    }
+
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list',[
+            'todos'=>Todo::latest()->where('name','like', "%{$this->search}%")->paginate(5)
+        ]);
     }
 }
